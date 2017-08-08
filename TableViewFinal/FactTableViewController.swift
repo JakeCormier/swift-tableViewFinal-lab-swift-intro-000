e278a21a516476ea1d12455ba82c42b359e03140
JakeCormier <jake.r.cormier@gmail.com>
2017-08-08 12:28:17 -0600

    Complete problems

--- TableViewFinal/FactTableViewController.swift
+++ TableViewFinal/FactTableViewController.swift
@@ -9,4 +9,22 @@
 import UIKit
 
 class FactTableViewController: UITableViewController {
+    let factCellIdentifier: String = "FactCell"
+    
+    var planet: Planet!
+    
+    override func viewDidLoad() {
+        navigationItem.title = planet.name
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return planet.facts.count
+    }
+    //will override func tableView(_:cellForRatAt:) after completing the implementation of FactTableViewCell ////// MUST BUILD THE INTERFACE FIRST
+    
 }
